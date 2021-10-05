e4ffbe7e1c829da90dcf4b482b4f741ad856d781
Maxim Wolotschij <m.wolotschij@web.de>
2021-10-05 15:59:41 +0200

    updated code - added bootstrap and refactored the controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,6 @@
 class QuestionsController < ApplicationController
+  def ask; end
+
   def answer
     @question = params[:question]
     @answer = coach_answer
@@ -7,9 +9,11 @@ class QuestionsController < ApplicationController
   private
 
   def coach_answer
-    if @question == 'I am going to work'
+    question = params[:question]
+
+    if question == 'I am going to work'
       'Great!'
-    elsif @question.end_with?('?')
+    elsif question.end_with?('?')
       'Silly question, get dressed and go to work!'
     else
       "I don't care, get dressed and go to work!"
